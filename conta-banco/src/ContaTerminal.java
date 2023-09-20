aaea0a28da44f9966b549f01e58efcb8f6671155
Curufiny <rafa.bassman@gmail.com>
2023-09-20 16:37:01 -0300

    Commit final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Estou mudando para confirmar commit");
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        Double saldo;
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da conta!");
+        numero = sc.nextInt();
+
+        System.out.println("Agora digite seu saldo! ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + "já está disponível para saque.");
+
     }
 }

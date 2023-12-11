728608dc4d30e0dc31a25a0e7997788d0b36c481
Rafael Gomes <rafa.bassman@gmail.com>
2023-12-11 16:28:14 -0300

    Add files via upload

---
+++ BOOTCAMP/src/mentoria.java
@@ -0,0 +1,23 @@
+package br.com.dio.desafio.dominio;
+
+
+public class Mentoria extends Conteudo{
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\''
+                '}';
+    }
+}
